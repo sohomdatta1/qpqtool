ab8007f9cfb9df45089e80f1e877dde987789599
Sohom <sohomdatta1+git@gmail.com>
2024-02-13 17:46:48 -0500

    add caching

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
 from flask import Flask
 from db import get_conn
 from bleach import clean
+import json
+from datetime import datetime, timedelta
+from redis_init import rediscl as r, REDIS_KEY_PREFIX
 
 app = Flask(__name__)
 
 @app.get('/qpq/<path:username>')
 def qpq( username: str ):
+    cached_val = r.get(REDIS_KEY_PREFIX + username)
+    if cached_val == None:
+        offset = 0
+        cached_response = []
+    else:
+        offset = int(cached_val.split(b'|', 1)[0])
+        cached_response = json.loads( cached_val.split(b'|',1)[1] )
     with get_conn().cursor() as cursor:
         cursor.execute("""SELECT p.page_title
         FROM page p 
         JOIN revision r ON p.page_id = r.rev_page
         AND p.page_namespace = 10
+        AND r.rev_timestamp > %s
         AND r.rev_parent_id = '0'
         AND p.page_is_redirect = '0'
         AND r.rev_actor IN (SELECT actor_id FROM actor WHERE actor_name = %s)
         AND p.page_title LIKE %s
         GROUP BY page_title
-        """, (username, 'Did_you_know_nominations/%'))
+        """, (int(offset), username, 'Did_you_know_nominations/%'))
         pages = cursor.fetchall()
-        resp_json = []
+        print(len(pages))
+        resp_json = cached_response
         for page in pages:
             resp_json.append('Template:' + page[0].decode())
+        tmstmp = datetime.now().strftime("%Y%m%d%H%M%S")
+        r.set(REDIS_KEY_PREFIX + username, tmstmp + '|' + json.dumps(resp_json))
+        r.expire(REDIS_KEY_PREFIX + username, timedelta(days=365 * 2))
         if len(resp_json) == 0:
             return '<h1>No DYK nominations found</h1>'
         
@@ -31,8 +46,8 @@ def qpq( username: str ):
 
         if len(resp_json) < 5:
             return '<h1> Less than 5 DYK nominations found</h1>\n' + listofdyknoms
-        else:
-            return f'<h1>QPQ required, these are all of {clean(username)}\'s DYK noms</h1>' + listofdyknoms
+        
+        return f'<h1>QPQ required, these are all of {clean(username)}\'s DYK noms</h1>' + listofdyknoms
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
---
+++ redis_init.py
@@ -0,0 +1,20 @@
+import os
+import redis
+
+redis_url = 'redis://localhost:6379/9'
+
+if 'NOTDEV' in os.environ:
+    redis_url = 'redis://redis.svc.tools.eqiad1.wikimedia.cloud:6379/0'
+elif 'DOCKER' in os.environ:
+    redis_url = 'redis://redis:6379/9'
+
+rediscl = redis.Redis(host='localhost', port=6379, db=9)
+
+
+if 'NOTDEV' in os.environ:
+    rediscl = redis.Redis(
+        host='redis.svc.tools.eqiad1.wikimedia.cloud',
+        port=6379,
+        db=0)
+
+REDIS_KEY_PREFIX = 'mw-toolforge-qpqtool'
