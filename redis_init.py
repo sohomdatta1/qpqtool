dfd65f21b618e3ffdb73d091549524bf748493ce
Sohom <sohomdatta1+git@gmail.com>
2024-02-15 06:10:54 -0500

    do not keep lingering connections, causes timeouts

--- redis_init.py
+++ redis_init.py
@@ -8,13 +8,16 @@ if 'NOTDEV' in os.environ:
 elif 'DOCKER' in os.environ:
     redis_url = 'redis://redis:6379/9'
 
-rediscl = redis.Redis(host='localhost', port=6379, db=9)
+def get_redis_conn():
+
+    rediscl = redis.Redis(host='localhost', port=6379, db=9)
 
 
-if 'NOTDEV' in os.environ:
-    rediscl = redis.Redis(
-        host='redis.svc.tools.eqiad1.wikimedia.cloud',
-        port=6379,
-        db=0)
+    if 'NOTDEV' in os.environ:
+        rediscl = redis.Redis(
+            host='redis.svc.tools.eqiad1.wikimedia.cloud',
+            port=6379,
+            db=0)
+    return rediscl
 
 REDIS_KEY_PREFIX = 'mw-toolforge-qpqtool'
