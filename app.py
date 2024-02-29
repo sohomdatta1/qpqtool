cc2d6824c32a1311ed9ab57049650f3ba4f2289a
Sohom <sohomdatta1+git@gmail.com>
2024-02-28 22:43:28 -0500

    Add credits URL

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from db import get_conn
 from bleach import clean
 import json
 from datetime import datetime, timedelta
+import re
 from redis_init import get_redis_conn, REDIS_KEY_PREFIX
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ def dyk():
 @app.get('/qpq/<path:username>')
 def qpq( username: str ):
     with get_redis_conn() as r:
-        cached_val = r.get(REDIS_KEY_PREFIX + username)
+        cached_val = r.get(REDIS_KEY_PREFIX + 'qpq' + username)
         if cached_val == None:
             offset = 0
             cached_response = []
@@ -36,14 +37,15 @@ def qpq( username: str ):
             AND r.rev_actor IN (SELECT actor_id FROM actor WHERE actor_name = %s)
             AND p.page_title LIKE %s
             GROUP BY page_title
+            ORDER BY r.rev_timestamp
             """, (int(offset), username, 'Did_you_know_nominations/%'))
             pages = cursor.fetchall()
             resp_json = cached_response
             for page in pages:
                 resp_json.append('Template:' + page[0].decode())
             tmstmp = datetime.now().strftime("%Y%m%d%H%M%S")
-            r.set(REDIS_KEY_PREFIX + username, tmstmp + '|' + json.dumps(resp_json))
-            r.expire(REDIS_KEY_PREFIX + username, timedelta(days=365 * 2))
+            r.set(REDIS_KEY_PREFIX + 'qpq' + username, tmstmp + '|' + json.dumps(resp_json))
+            r.expire(REDIS_KEY_PREFIX + 'qpq' + username, timedelta(days=365 * 2))
             if len(resp_json) == 0:
                 return '<h1>No DYK nominations found</h1>'
             
@@ -56,6 +58,51 @@ def qpq( username: str ):
                 return '<h1> Less than 5 DYK nominations found</h1>\n' + listofdyknoms
             
             return f'<h1>QPQ required, these are all of {clean(username)}\'s DYK noms</h1>' + listofdyknoms
+        
+@app.get('/credits/<path:username>')
+def credits(username):
+    username = username.replace(' ', '_')
+    with get_redis_conn() as r:
+        cached_val = r.get(REDIS_KEY_PREFIX + 'credits' + username)
+        if cached_val == None:
+            cached_response = []
+        else:
+            cached_response = json.loads( cached_val.split(b'|',1)[1] )
+        resp_json = []
+        if cached_response == []:
+            with get_conn().cursor() as cursor:
+                cursor.execute("""SELECT c.comment_text
+                FROM page p 
+                JOIN revision r ON p.page_id = r.rev_page
+                JOIN comment c ON r.rev_comment_id = c.comment_id
+                AND p.page_namespace = 3
+                AND p.page_title = %s
+                AND r.rev_actor = 24447
+                ORDER BY r.rev_timestamp;
+                """, (username))
+                comments = cursor.fetchall()
+                for comment in comments:
+                    try:
+                        page = re.search(r'Giving DYK credit for \[\[([^\]]+)\]\] on behalf of \[\[([^\]]+)\]\]', comment[0].decode()).groups()[0]
+                        resp_json.append(page)
+                    except Exception as _:
+                        print(_)
+                        pass
+        else:
+            resp_json = cached_response
+        tmstmp = datetime.now().strftime("%Y%m%d%H%M%S")
+        r.set(REDIS_KEY_PREFIX + 'credits' + username, tmstmp + '|' + json.dumps(resp_json))
+        r.expire(REDIS_KEY_PREFIX + 'credits' + username, timedelta(days=365 * 2))
+        if len(resp_json) == 0:
+            return '<h1>No DYK nominations found</h1>'
+        
+        list_html = []
+        for page in resp_json:
+            list_html.append( f'<li>At <a href="https://en.wikipedia.org/wiki/Template:Did_you_know_nominations/{page}" target="_blank">Template:Did you know nominations/{page}</a> ' + 
+                                f'for <a href="https://en.wikipedia.org/wiki/{page}" target="_blank">{page}</a></li>')
+        listofdyknoms = '<ol>' + '\n'.join(list_html) + '</ol>'
+        
+        return f'<h1>DYK credits given to {clean(username)}</h1>' + listofdyknoms
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
